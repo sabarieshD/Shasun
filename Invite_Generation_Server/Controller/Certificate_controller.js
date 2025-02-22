358546ae2393ddf9e25ecc379a0907e73aa520ee
Sabariesh D <109064056+sabarieshD@users.noreply.github.com>
2025-02-22 13:31:43 +0530

    Update Certificate_controller.js

--- Invite_Generation_Server/Controller/Certificate_controller.js
+++ Invite_Generation_Server/Controller/Certificate_controller.js
@@ -4,10 +4,10 @@ const path = require('path');
 const multer = require('multer');
 
 // Set up Multer for file uploads
-const uploadDir = path.join(__dirname, './certificates');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+// const uploadDir = path.join(__dirname, './certificates');
+// if (!fs.existsSync(uploadDir)) {
+//     fs.mkdirSync(uploadDir, { recursive: true });
+// }
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
